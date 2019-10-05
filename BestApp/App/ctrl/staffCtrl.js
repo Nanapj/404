815a6c92401d36381efd5518bd5fb96d89feb21f
PhilTran1994 <tranhoangphianh1994@gmail.com>
2019-10-05 10:29:06 +0700

    Get update ID from respository Int to GUID

--- BestApp/App/ctrl/staffCtrl.js
+++ BestApp/App/ctrl/staffCtrl.js
@@ -1,22 +1,47 @@
 'use strict';
 
 angular.module('app')
-    .controller('StaffCtrl', ['$scope', '$state', '$http', function ($scope, $state, $http){
+    .controller('StaffCtrl', ['$scope', '$state', '$http', function ($scope, $state, $http, $rootScope){
         console.log('StaffCtrl loaded!');
         var _url = "/odata/Staffs";
         var vm = this;
-
+        vm.access_token = localStorage.getItem('access_token');
         vm.model = {};
         vm.model.HasAccount = false;
         vm.toolbarTemplate = toolbarTemplate;
         vm.create = create;
+        vm.edit = edit;
         
         function create(){
             $state.go('app.staff.create');
         }
 
+        function edit(){
+            $state.go('app.staff.edit');
+        }
+
         function toolbarTemplate() {
             return kendo.template($("#toolbar").html());
+        }
+
+        vm.createSubmit = function () {
+            console.log(vm.access_token);
+            $http({
+                url: _url,
+                method: 'POST',
+                data: JSON.stringify(vm.model),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer '+ vm.access_token.replace(/['"]+/g, '')
+                },
+            }).then(function(response){
+                console.log(response);
+                alert("đã tạo");
+            });
+        }
+
+        vm.submit = function(){
+            alert('Submit button clicked');
         }
 
         $scope.mainGridOptions = {
