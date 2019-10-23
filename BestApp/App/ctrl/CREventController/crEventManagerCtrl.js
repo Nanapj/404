170b27f57348f9279788d8320437f1591e2129f0
PhilTran1994 <tranhoangphianh1994@gmail.com>
2019-10-23 18:06:48 +0700

    create 50% Ui of Event managerment page

--- BestApp/App/ctrl/CREventController/crEventManagerCtrl.js
+++ BestApp/App/ctrl/CREventController/crEventManagerCtrl.js
@@ -1,6 +1,46 @@
 'use strict';
 angular.module('app')
     .controller('crEventManagerCtrl', ['$scope', '$state', '$stateParams', '$http', 'toaster', function ($scope, $state, $stateParams, $http, toaster){
-        
+        var vm = this; 
+        vm.handleDateRange = handleDateRange;
+        vm.tagsList = [
+            { "Name" : "Nút 1" , "Id" : "1"},
+            { "Name" : "Nút 2" , "Id" : "2"},
+            { "Name": "Nút 3" , "Id" : "3"}
+        ];
+        $(".editButton").kendoButton({
+            icon: "edit"
+        });
+
+        $(".closeButton").kendoButton({
+            icon: "close"
+        });
+        kendo.init("#listoftag");
+        vm.startDate = new Date();
+        vm.endDate = new Date();
+        function handleDateRange() {
+            console.log('Handle changed')
+        }
+        vm.departmentSelectedIds = [ 4, 7 ];
+        vm.departmentSelectOptions = {
+            placeholder: "Bộ phận...",
+            dataTextField: "ProductName",
+            dataValueField: "ProductID",
+            valuePrimitive: true,
+            autoBind: false,
+            dataSource: {
+                type: "odata",
+                serverFiltering: true,
+                transport: {
+                    read: {
+                        url: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Products",
+                    }
+                }
+            }
+        };
+        $scope.onDateRangeChange = function() {
+            console.log(vm.startDate);
+            console.log(vm.endDate);
+        };
     }
 ]);
