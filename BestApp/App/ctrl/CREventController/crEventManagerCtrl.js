ef3a4a2f1654f4d48f1af9a14ee7b61121e78e0d
PhilTran1994 <tranhoangphianh1994@gmail.com>
2019-10-24 17:34:49 +0700

    Done UI with event manager

--- BestApp/App/ctrl/CREventController/crEventManagerCtrl.js
+++ BestApp/App/ctrl/CREventController/crEventManagerCtrl.js
@@ -2,12 +2,31 @@
 angular.module('app')
     .controller('crEventManagerCtrl', ['$scope', '$state', '$stateParams', '$http', 'toaster', function ($scope, $state, $stateParams, $http, toaster){
         var vm = this; 
+        var _url = "/odata/Staffs";
         vm.handleDateRange = handleDateRange;
+        vm.selectedStaff = {};
         vm.tagsList = [
-            { "Name" : "Nút 1" , "Id" : "1"},
-            { "Name" : "Nút 2" , "Id" : "2"},
-            { "Name": "Nút 3" , "Id" : "3"}
+            { "Name" : "Tag A" , "Id" : "1"},
+            { "Name" : "Tag B" , "Id" : "2"},
+            { "Name": "Tag C" , "Id" : "3"}
         ];
+        vm.tagFilterList = [];
+        $scope.tagItemClicked = function(item,_this,index) {
+            var idButtonClicked = "#button"+item.Id;
+            if($(idButtonClicked).css("background-color") !== 'rgb(66, 133, 244)'){
+                $(idButtonClicked).css({
+                    "background-color" : "#4285F4"
+                });
+                $(idButtonClicked).addClass("tag-blue-color");     
+                vm.tagFilterList.push(item);
+            } else {
+                $(idButtonClicked).css({
+                    "background-color":"white"      
+                });
+                $(idButtonClicked).removeClass("tag-blue-color");   
+                vm.tagFilterList.splice(index,1);
+            }
+        };
         $(".editButton").kendoButton({
             icon: "edit"
         });
@@ -42,5 +61,69 @@ angular.module('app')
             console.log(vm.startDate);
             console.log(vm.endDate);
         };
+        $scope.mainGridOptions = {
+            dataSource: {
+                type: "odata-v4",
+                transport: {
+                    read: _url
+                },
+                pageSize: 50,
+                serverPaging: true,
+                serverSorting: true
+            },
+            sortable: true,
+            pageable: true,
+            height: 500,
+            dataBound: onDataBound,
+            change: onChange,
+            columns: [
+                {
+                    field: "FullName",
+                    title: "Họ tên",
+                    width: "50px"
+                },
+                {
+                    field: "Phone",
+                    title: "Số điện thoại",
+                    width: "50px"
+                },
+                {
+                    field: "Email",
+                    title: "Email",
+                    width: "50px"
+                },
+                {
+                    field: "Address",
+                    title: "Địa chỉ",
+                    width: "80px"
+                },
+                { command: [{ text: "Chi tiết", click: showDetails },{text: "Sửa", click: showDetails }], title: " ", width: "200px" }
+            ]
+        };
+        var wnd = $("#details")
+                        .kendoWindow({
+                            title: "Chi tiết phiếu",
+                            modal: true,
+                            visible: false,
+                            resizable: false,
+                            width: 300
+                        }).data("kendoWindow");
+
+        var detailsTemplate = kendo.template($("#template").html());
+        function onDataBound(e) {
+            this.expandRow(this.tbody.find("tr.k-master-row").first());
+        }
+        function showDetails(e) {
+            e.preventDefault();
+
+            var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+            wnd.content(detailsTemplate(dataItem));
+            wnd.center().open();
+        }
+        function onChange(e) {
+            var grid = $('#staffgrid').data('kendoGrid');
+            var selectedItem = grid.dataItem(grid.select());
+            vm.selectedStaff = selectedItem;
+        }
     }
 ]);
--- BestApp/App/ctrl/CREventController/crReminderManagerCtrl.js
+++ BestApp/App/ctrl/CREventController/crReminderManagerCtrl.js
@@ -1,6 +1,77 @@
 'use strict';
 angular.module('app')
     .controller('crReminderManagerCtrl', ['$scope', '$state', '$stateParams', '$http', 'toaster', function ($scope, $state, $stateParams, $http, toaster){
-        
+        var vm = this;
+        var _url = "/odata/Staffs";
+        vm.model = {};
+        vm.selectedStaff = {};
+        vm.toolbarTemplate = toolbarTemplate;
+        function toolbarTemplate() {
+            return kendo.template($("#toolbar").html());
+        }
+        $scope.mainGridOptions = {
+            dataSource: {
+                type: "odata-v4",
+                transport: {
+                    read: _url
+                },
+                pageSize: 50,
+                serverPaging: true,
+                serverSorting: true
+            },
+            sortable: true,
+            pageable: true,
+            height: 900,
+            dataBound: onDataBound,
+            change: onChange,
+            columns: [
+                {
+                    field: "FullName",
+                    title: "Họ tên",
+                    width: "50px"
+                },
+                {
+                    field: "Phone",
+                    title: "Số điện thoại",
+                    width: "50px"
+                },
+                {
+                    field: "Email",
+                    title: "Email",
+                    width: "50px"
+                },
+                {
+                    field: "Address",
+                    title: "Địa chỉ",
+                    width: "80px"
+                },
+                { command: [{ text: "Chi tiết", click: showDetails },{text: "Sửa", click: showDetails }], title: " ", width: "200px" }
+            ]
+        };
+        var wnd = $("#details")
+                        .kendoWindow({
+                            title: "Chi tiết phiếu",
+                            modal: true,
+                            visible: false,
+                            resizable: false,
+                            width: 300
+                        }).data("kendoWindow");
+
+        var detailsTemplate = kendo.template($("#template").html());
+        function onDataBound(e) {
+            this.expandRow(this.tbody.find("tr.k-master-row").first());
+        }
+        function showDetails(e) {
+            e.preventDefault();
+
+            var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+            wnd.content(detailsTemplate(dataItem));
+            wnd.center().open();
+        }
+        function onChange(e) {
+            var grid = $('#staffgrid').data('kendoGrid');
+            var selectedItem = grid.dataItem(grid.select());
+            vm.selectedStaff = selectedItem;
+        }
     }
 ]);
